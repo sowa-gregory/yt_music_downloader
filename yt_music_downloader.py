05a6cb1c5013cd51b65faadc14b458c991c4ca0d
Grzegorz Sowa <sowa.gregory@gmail.com>
2018-12-23 02:33:31 +0100

    update

--- yt_music_downloader.py
+++ yt_music_downloader.py
@@ -9,26 +9,35 @@ flask = Flask(__name__)
 
 
 class DownloadThread(threading.Thread):
-    def __init__(self, queue, song_id, url):
+    def __init__(self, queue, song_id):
         super(DownloadThread, self).__init__()
-        self.stopRequest = threading.Event()
         self.queue = queue
         self.song_id = song_id
-        self.url = url
+
+    def onDownload(self, progress):
+        status = progress['status']
+        total_bytes = progress['total_bytes']
+
+        if(status == "finished"):
+            downloaded_bytes = total_bytes
+            eta = 0
+        else:
+            downloaded_bytes = progress['downloaded_bytes']
+            eta = progress['eta']
+
+        song_info = {"filename": progress['filename'], "downloaded_bytes": downloaded_bytes,
+                     "total_bytes": total_bytes, "eta": eta}
+
+        data = {
+            "thread_id": threading.get_ident(), "status": progress['status'], "song_id": self.song_id, "data": song_info}
+        self.queue.put(data)
 
     def run(self):
-        counter = 1
-        while(not self.stopRequest.is_set()):
-            data = {"thread_id": threading.get_ident(), "song_id": self.song_id, "data": {
-                "url": self.url, "counter": counter}}
-            self.queue.put(data)
-            counter = counter+1
-            time.sleep(0.1)
-        print("download thread exit")
-
-    def stop(self):
-        self.stopRequest.set()
-        
+        ydl_opts = {'quiet': 'true', 'format': 'bestaudio/best',
+                    'progress_hooks': [self.onDownload]}
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(["https://youtu.be/"+self.song_id])
+        print("download thread exit\n")
 
 
 class DownloadManager(threading.Thread):
@@ -39,23 +48,55 @@ class DownloadManager(threading.Thread):
         self.threads = {}
         self.download_status = {}
         self.locker = threading.Lock()
+        self.downloadDict = {}
+        self.maxThreads = 2
+
+        # start manager
         self.start()
+        
+
+    def scanToDownload(self):
+        print(self.downloadDict)
+        self.downloadDict["cJpVlFMHz1E"] = False
+        self.downloadDict["pRpeEdMmmQ0"] = False
+
+    def getFirstElement(self):
+        for key in self.downloadDict.keys():
+            if(self.downloadDict[key] == False):
+                self.downloadDict[key] = True
+                return key
+        return None
+
+    def startDownload(self):
+        
+        if(len(self.threads) < self.maxThreads):
+            song_id = self.getFirstElement()
+            if(song_id != None):
+                thr = DownloadThread(self.queue, song_id)
+                thr.start()
+                self.threads[thr.ident] = thr
+                return True
+        return False
 
     def run(self):
-        thr = DownloadThread(self.queue, "1234", "asdfasdfasf")
-        thr.start()
-        self.threads[thr.ident] = thr
-
-        time.sleep(0.5)
-        thr = DownloadThread(self.queue, "9877", "blekota")
-        thr.start()
-        self.threads[thr.ident] = thr
+        self.scanToDownload()
+        while( self.startDownload() == True ):
+            pass
+      
 
         while(not self.stopRequest.is_set()):
+            print("tick")
             try:
-                data = self.queue.get(timeout=1)
+                data = self.queue.get(timeout=5)
+                print(data)
+      
                 with self.locker:
                     self.download_status[data['song_id']] = data['data']
+
+                if(data['status'] == "finished"):
+                    del self.threads[data['thread_id']]
+                    print( "start down"+str(self.startDownload()))
+      
             except queue.Empty:
                 pass
         print("manager thread exit")
@@ -66,8 +107,6 @@ class DownloadManager(threading.Thread):
         return status
 
     def trystop(self):
-        for id in self.threads.keys():
-            self.threads[id].stop()
         for id in self.threads.keys():
             if(not self.threads[id].isAlive()):
                 self.threads[id].join()
@@ -84,7 +123,7 @@ class DownloadManager(threading.Thread):
                 stopped = True
             except KeyboardInterrupt:
                 pass
-       
+
 
 # Queue of files is stored in separate files with name:
 # - URL.to_download - files queued to be downloaded (both not started yet and partially downloaded)
@@ -122,6 +161,7 @@ def download_test():
     ydl_opts = {
         'quiet': 'true',
         'format': 'bestaudio/best',
+        'outtmpl': '%(title)s.%(ext)s',
         'progress_hooks': [on_download]
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
